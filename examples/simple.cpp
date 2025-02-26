138da043ef49c812ea917104af20165784696169
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2025-02-26 09:33:38 -0500

    Completely re-organize the name generator. Add title generation.

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -1,21 +1,24 @@
 #include "namegen/namegen.hpp"
 
-#include <cstdint>
+#include <chrono>
+#include <iomanip>
 #include <iostream>
+
+void generate_and_print(const std::string &pattern)
+{
+    std::size_t seed = static_cast<std::size_t>(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+    std::string name = namegen::generate(pattern, seed);
+    std::cout << "| " << std::setw(32) << std::right << pattern << " |";
+    std::cout << " " << std::setw(32) << std::right << name << " |\n";
+}
 
 int main(int, char *[])
 {
-    std::string name;
-    std::string pattern = "!ssV'!i";
-    uint64_t seed       = static_cast<uint64_t>(time(NULL));
-
-    int ret = namegen::generate(name, pattern, seed);
-
-    std::cout << "Name    : " << name << "\n";
-    std::cout << "Pattern : " << name << "\n";
-    std::cout << "Seed    : " << seed << "\n";
-    std::cout << "Code    : " << ret << "\n";
-    std::cout << "Size    : " << name.size() << "\n";
-    std::cout << "Length  : " << name.length() << "\n";
+    generate_and_print("!ssV'!i");
+    generate_and_print("v!M");
+    generate_and_print("c(dim)");
+    generate_and_print("C!i");
+    generate_and_print("<(C!i)|(v!M)>");
+    generate_and_print("<C!i|v!M|>");
     return 0;
 }
